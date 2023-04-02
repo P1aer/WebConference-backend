8aa10303fd02c23689fc6f10f7cd19636d75a087
Ilia Osipov <iosipov@emphasoft.com>
2023-04-02 20:07:51 +0300

    fix logic for room connect

---
+++ config/actions.js
@@ -0,0 +1,10 @@
+export const ACTIONS = {
+    JOIN: 'join',
+    LEAVE: 'leave',
+    ADD_PEER: 'add-peer',
+    REMOVE_PEER: 'remove-peer',
+    RELAY_SDP: 'relay-sdp',
+    RELAY_ICE: 'relay-ice',
+    ICE_CANDIDATE: 'ice-candidate',
+    SESSION_DESCRIPTION: 'session-description',
+}
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors"
 import mongoose from "mongoose";
 import auth from "./routes/auth.js"
 import rooms from './routes/rooms.js'
-import Room from "./models/Room.js";
+import { ACTIONS } from "./config/actions.js";
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("start DB MONGA"))
 
@@ -32,116 +32,70 @@ io.use((socket, next) => {
 });
 
 
-let connections = {};
-/*io.on("connection", (socket) => {
-    console.log("User connected with id", socket.id);
-
-    socket.on("ready", async (peerId, peerType) => {
-        // Make sure that the hostname is unique, if the hostname is already in connections, send an error and disconnect
-        if (peerId in connections) {
-            socket.emit("uniquenessError", {
-                message: `${peerId} is already connected to the signalling server. Please change your peer ID and try again.`,
-            });
-            socket.disconnect(true);
-        } else {
-            console.log(`Added ${peerId} to connections`);
-            // Let new peer know about all exisiting peers
-            socket.send({ from: "all", target: peerId, payload: { action: "open", connections: Object.values(connections), bePolite: false } }); // The new peer doesn't need to be polite.
-            // Create new peer
-            const newPeer = { socketId: socket.id, peerId, peerType };
-            // Updates connections object
-            connections[peerId] = newPeer;
-            // Let all other peers know about new peer
-            socket.broadcast.emit("message", {
-                from: peerId,
-                target: "all",
-                payload: { action: "open", connections: [newPeer], bePolite: true }, // send connections object with an array containing the only new peer and make all exisiting peers polite.
-            });
-        }
-    });
-    socket.on("message", (message) => {
-        // Send message to all peers expect the sender
-        socket.broadcast.emit("message", message);
-    });
-    socket.on("messageOne", (message) => {
-        // Send message to a specific targeted peer
-        const { target } = message;
-        const targetPeer = connections[target];
-        if (targetPeer) {
-            io.to(targetPeer.socketId).emit("message", { ...message });
-        } else {
-            console.log(`Target ${target} not found`);
-        }
-    });
-    socket.on("disconnect", () => {
-        const disconnectingPeer = Object.values(connections).find((peer) => peer.socketId === socket.id);
-        if (disconnectingPeer) {
-            console.log("Disconnected", socket.id, "with peerId", disconnectingPeer.peerId);
-            // Make all peers close their peer channels
-            socket.broadcast.emit("message", {
-                from: disconnectingPeer.peerId,
-                target: "all",
-                payload: { action: "close", message: "Peer has left the signaling server" },
-            });
-            // remove disconnecting peer from connections
-            delete connections[disconnectingPeer.peerId];
-        } else {
-            console.log(socket.id, "has disconnected");
-        }
-    });
-});*/
 io.on("connection", (socket) => {
-    socket.emit("me", socket.id);
+    const leaveRoom = () => {
 
-    socket.on("disconnect", async () => {
-        console.log(socket.currentRoom, 'disconnect')
-        if (socket.currentRoom) {
-           await Room.findByIdAndUpdate(socket.currentRoom, {
-                $pull: {
-                    users: socket.userId
-                }
+        const {rooms} = socket;
+        // TODO тут мб что поменять
+        Array.from(rooms).filter(roomId => roomId!== socket.id).forEach(roomId => {
+
+            const clients = Array.from(io.sockets.adapter.rooms.get(socket.currentRoom) || [])
+            clients.forEach(clientID => {
+                // оповещаем о выходе пользователя
+                io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+                    peerID: socket.id,
+                })
+                // на фронте себе
+                socket.emit(ACTIONS.REMOVE_PEER, {
+                    peerID: clientID
+                })
             })
-            socket.leave(socket.currentRoom)
-        }
+
+            socket.leave(roomId)
+        })
+
+
         socket.currentRoom = null
+    }
+    socket.on(ACTIONS.JOIN ,async (roomId) => {
+        const {rooms: joinedRooms} = socket;
 
-        socket.broadcast.emit("callEnded");
-    });
-
-    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-    });
-
-    socket.on("answerCall", (data) => {
-        io.to(data.to).emit("callAccepted", data.signal);
-    });
-    socket.on('ROOM:JOIN',async (roomId) => {
-        console.log(socket.currentRoom,'room')
-        if (socket.currentRoom) {
-            await Room.findByIdAndUpdate(socket.currentRoom, {
-                $pull: {
-                    users: socket.userId
-                }
-            })
-            socket.leave(socket.currentRoom)
+        if (Array.from(joinedRooms).includes(roomId)) {
+            return console.warn(`Already joined to ${roomId}`);
         }
-       await Room.findByIdAndUpdate(roomId, {
-            $addToSet: {
-                users: socket.userId
-            }
+        console.log(roomId, 'Id on join')
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || [])
+        console.log(clients, 'client in room')
+        clients.forEach(clientID => {
+            // те кто в комнате принимают пир
+            io.to(clientID).emit(ACTIONS.ADD_PEER, {
+                peerID: socket.id,
+                createOffer: false,
+            })
+            // кто подключился создает офер
+            socket.emit(ACTIONS.ADD_PEER, {
+                peerID: clientID,
+                createOffer: true,
+            })
         })
         socket.currentRoom = roomId
-        console.log(socket.currentRoom,'room2')
         socket.join(roomId)
+        console.log(io.sockets.adapter.rooms.get(roomId),'clients after join\n ////////////////////////////////////////')
     })
-    socket.on('ROOM:LEAVE',async ( roomId ) => {
-        await Room.findByIdAndUpdate(roomId, {
-            $pull: {
-                users: socket.userId
-            }
+    socket.on("disconnecting", leaveRoom)
+    socket.on(ACTIONS.LEAVE, leaveRoom)
+
+    socket.on(ACTIONS.RELAY_SDP,({peerID,sessionDescription}) => {
+        io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+            peerID: socket.id,
+            sessionDescription
         })
-        socket.leave(roomId)
-        socket.currentRoom = null
+    })
+    socket.on(ACTIONS.RELAY_ICE,({peerID, iceCandidate}) => {
+        io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+            peerID: socket.id,
+            iceCandidate,
+        })
     })
 });
 server.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
